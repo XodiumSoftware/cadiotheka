0bc753fb32d692efecdb7a0667d866137d2559e4
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-07-20 18:41:04 +0200

    Add "My projects" link to account menu
    Clicking the new menu item closes the dropdown and populates the search
    query with the current user's `@author:` handle.

--- cadiotheka-frontend/src/components/sections/header.rs
+++ cadiotheka-frontend/src/components/sections/header.rs
@@ -524,6 +524,22 @@ pub fn Header() -> impl IntoView {
                                                         </button>
                                                     </li>
                                                     <li role="none">
+                                                        <button
+                                                            type="button"
+                                                            class="w-full text-left px-4 py-2 hover:bg-base-content/10"
+                                                            role="menuitem"
+                                                            on:click=move |_| {
+                                                                set_account_menu_open.set(false);
+                                                                if let Some(account) = current_user.account.get() {
+                                                                    let author_query = format!("@author:{}", account.username);
+                                                                    search.set_query.set(author_query);
+                                                                }
+                                                            }
+                                                        >
+                                                            {move || t_string!(i18n, account.my_projects)}
+                                                        </button>
+                                                    </li>
+                                                    <li role="none">
                                                         <a
                                                             href={encode_redirect_url(&auth_url("/logout"))}
                                                             class="block w-full text-left px-4 py-2 hover:bg-base-content/10 text-error"
